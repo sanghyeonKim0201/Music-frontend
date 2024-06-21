6fb27b4f24e7903f74cdad455b7223c2f51cb982
sanghyeonkim <gmhty4345@gmail.com>
2024-06-21 11:27:29 +0900

    🚩 : main layout useEffect add

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,6 +4,7 @@ import MusicBar from '@/components/musicBar';
 import { menuSlice } from '@/lib/feature/menuSlice';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
 import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function MainLayout({
   children,
@@ -15,28 +16,30 @@ export default function MainLayout({
   );
   const dispatch = useAppDispatch();
   const pathname = usePathname();
-  const menu = [
-    {
-      key: '/library',
-      value: 'library',
-    },
-    {
-      key: '/main',
-      value: 'home',
-    },
-    {
-      key: '/explore',
-      value: 'explore',
-    },
-  ];
 
-  if (menu.find((o) => pathname.startsWith(o.key))) {
-    dispatch(
-      menuSlice.actions.selectionMenu(
-        menu.find((o) => pathname.startsWith(o.key))?.value,
-      ),
-    );
-  }
+  useEffect(() => {
+    const menu = [
+      {
+        key: '/library',
+        value: 'library',
+      },
+      {
+        key: '/main',
+        value: 'home',
+      },
+      {
+        key: '/explore',
+        value: 'explore',
+      },
+    ];
+    if (menu.find((o) => pathname.startsWith(o.key))) {
+      dispatch(
+        menuSlice.actions.selectionMenu(
+          menu.find((o) => pathname.startsWith(o.key))?.value,
+        ),
+      );
+    }
+  }, [pathname, dispatch]);
 
   return (
     // 사이드바 펴치면 ml-40 접히면 ml-0
